a6da1f9a3fe278efdaf3c0287d2af0676fa75416
ErikAlvesAlmeida <eriikalves55@gmail.com>
2024-09-21 18:17:30 -0300

    added Cors to the project

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("./conn/conn");
+const cors = require("cors");
 const auth = require("./routes/auth");
 const list = require("./routes/list");
 app.use(express.json());
